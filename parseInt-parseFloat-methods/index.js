591654c59cc144de3f4bf40e9432be8bd59c90c7
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-06-15 22:54:07 +0300

    parseInt-parseFloat

---
+++ parseInt-parseFloat-methods/index.js
@@ -0,0 +1,25 @@
+
+// Play-(1)
+//------------------------------------
+let a = 1; 
+let b = '22';
+sum = a+b;
+// Output is 122 becuase concetenate!
+console.log(sum); 
+
+
+// Play-(2)
+//------------------------------------
+// parseInt()
+let bAgain = parseInt(b);
+sum = a+bAgain;
+// Output is 23 
+console.log(sum); 
+
+// Play-(3)
+//------------------------------------
+// parseInt()
+let bFloat = parseFloat("53.4456");
+sum = a+bFloat;
+// Output is 54.4456
+console.log(sum); 
